bb2fd623d6a7ccb6f8af899edafcb5d46d162ffc
A.Voi <voytsishevskiy@tochka.com>
2020-03-31 16:21:30 +0500

    Initial commit

---
+++ src/main/scala/io/funfunfine/gde/Main.scala
@@ -0,0 +1,9 @@
+package io.funfunfine.gde
+
+import cats.effect.{ExitCode, IO, IOApp}
+import cats.implicits._
+
+object Main extends IOApp {
+  def run(args: List[String]) =
+    GdeServer.stream[IO].compile.drain.as(ExitCode.Success)
+}
